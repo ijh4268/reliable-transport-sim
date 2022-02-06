6739f12925802e792b24159bfc1d8f3f357b2764
Isaac <isaachenry2022@u.northwestern.edu>
2022-02-06 15:40:02 -0600

    PART 3 STAGE A

--- streamer.py
+++ streamer.py
@@ -1,4 +1,5 @@
 # do not import anything else from loss_socket besides LossyUDP
+from concurrent.futures import ThreadPoolExecutor
 from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
@@ -15,9 +16,13 @@ class Streamer:
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
+        self.closed = False
         self.seq = 0
         self.expected_seq = 0
         self.recv_buf = []
+
+        executor = ThreadPoolExecutor(max_workers=1)
+        executor.submit(self.listener)
 
     def _chunk(self, data_bytes: bytes, size: int):
         for i in range(0, len(data_bytes), size):
@@ -25,9 +30,18 @@ class Streamer:
 
     def _fetch_data(self):
         data, addr = self.socket.recvfrom()
-        unpacked_data = struct.unpack(f'!i{len(data) - 4}s', data)
-        heapq.heappush(self.recv_buf, unpacked_data)
+        # ! -4 from len(data) to account for header size
+        if data:
+            unpacked_data = struct.unpack(f'!i{len(data) - 4}s', data)
+            heapq.heappush(self.recv_buf, unpacked_data)
 
+    def listener(self):
+        while not self.closed:
+            try:
+                self._fetch_data()
+            except Exception as e:
+                print("ERROR: listener died!")
+                print(e)
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
@@ -45,10 +59,8 @@ class Streamer:
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
         # your code goes here!  The code below should be changed!
-        data = None
         # this sample code just calls the recvfrom method on the LossySocket
-        while True:
-            # ? Subtract 4 for sequence number offset
+        while not self.closed:
             if self.recv_buf:
                 curr_packet = heapq.heappop(self.recv_buf)
                 # check if we have the packet we need
@@ -57,12 +69,10 @@ class Streamer:
                     return curr_packet[1] # return the data portion of the packet
                 else:
                     heapq.heappush(self.recv_buf, curr_packet)
-                    self._fetch_data()
-            else:
-                self._fetch_data()
             
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
         # your code goes here, especially after you add ACKs and retransmissions.
-        pass
+        self.closed = True
+        self.socket.stoprecv()
