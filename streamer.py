a18ffcc447d5e8c1c0a81e7660937672bf4861e0
Isaac <isaachenry2022@u.northwestern.edu>
2022-02-06 14:57:47 -0600

    STAGE 2

--- streamer.py
+++ streamer.py
@@ -3,6 +3,8 @@ from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
 import struct
+import heapq
+
 
 class Streamer:
     def __init__(self, dst_ip, dst_port,
@@ -13,10 +15,18 @@ class Streamer:
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
+        self.seq = 0
+        self.expected_seq = 0
+        self.recv_buf = []
 
-    def chunk(self, data_bytes: bytes, size: int):
+    def _chunk(self, data_bytes: bytes, size: int):
         for i in range(0, len(data_bytes), size):
             yield data_bytes[i:i+size]
+
+    def _fetch_data(self):
+        data, addr = self.socket.recvfrom()
+        unpacked_data = struct.unpack(f'!i{len(data) - 4}s', data)
+        heapq.heappush(self.recv_buf, unpacked_data)
 
 
     def send(self, data_bytes: bytes) -> None:
@@ -24,27 +34,33 @@ class Streamer:
         # Your code goes here!  The code below should be changed!
         packet_size = 1472 # ! packet size is 1472 bytes
         # * if the data_bytes is larger than the size of a packet, then we need to chunk it. 
-        data_chunks = list(self.chunk(data_bytes, packet_size))
+        # ! -4 from packet size to account for header (4 byte seq int)
+        data_chunks = list(self._chunk(data_bytes, packet_size-4))
         # for now I'm just sending the raw application-level data in one UDP payload
-        seq = 0
         for chunk in data_chunks:
-            packet = struct.pack(f'!i{len(chunk)}s', seq, chunk)
+            packet = struct.pack(f'!i{len(chunk)}s', self.seq, chunk)
             self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+            self.seq += 1
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
         # your code goes here!  The code below should be changed!
-        recv_buf = []
+        data = None
         # this sample code just calls the recvfrom method on the LossySocket
-        data, addr = self.socket.recvfrom()
-        curr_seq = 0
-        while data:
+        while True:
             # ? Subtract 4 for sequence number offset
-            recv_buf.append(struct.unpack(f'i{len(data) - 4}s', data))
+            if self.recv_buf:
+                curr_packet = heapq.heappop(self.recv_buf)
+                # check if we have the packet we need
+                if curr_packet[0] == self.expected_seq:
+                    self.expected_seq += 1 # increment expected sequence number
+                    return curr_packet[1] # return the data portion of the packet
+                else:
+                    heapq.heappush(self.recv_buf, curr_packet)
+                    self._fetch_data()
+            else:
+                self._fetch_data()
             
-            # For now, I'll just pass the full UDP payload to the app
-            return data
-
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
